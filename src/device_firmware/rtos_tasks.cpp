93a700be2adc511f87d01d81704237e07054a249
Karla Sanchez <karlasanchz@tamu.edu>
2025-12-08 00:18:04 -0600

    rtos files implemented

---
+++ src/device_firmware/rtos_tasks.cpp
@@ -0,0 +1,99 @@
+#include <pthread.h>
+#include <atomic>
+#include <unistd.h>
+#include <iostream>
+#include <vector>
+#include "audio_input.h"
+#include "ipc_config.h"
+#include "rtos_tasks.h"
+
+// global rtos state
+static std::atomic<bool> rtos_running{false};
+static pthread_t audio_receive_thread;
+static pthread_t signal_process_thread;
+static pthread_t classification_thread;
+static pthread_t output_thread;
+
+// task entry points
+void* audio_receive_task(void*);
+void* signal_process_task(void*);
+void* classification_task(void*);
+void* output_task(void*);
+
+// start rtos tasks
+bool start_rtos_tasks() {
+    rtos_running = true;
+    int err = 0;
+    // create threads with default scheduling for now
+    err |= pthread_create(&audio_receive_thread, nullptr, audio_receive_task, nullptr);
+    err |= pthread_create(&signal_process_thread, nullptr, signal_process_task, nullptr);
+    err |= pthread_create(&classification_thread, nullptr, classification_task, nullptr);
+    err |= pthread_create(&output_thread, nullptr, output_task, nullptr);
+    if (err) {
+        std::cerr << "[RTOS] Error starting tasks." << std::endl;
+        rtos_running = false;
+        return false;
+    }
+    std::cout << "[RTOS] All tasks started." << std::endl;
+    return true;
+}
+
+// request shutdown
+void request_rtos_shutdown() {
+    rtos_running = false;
+}
+
+// join all tasks
+void join_rtos_tasks() {
+    pthread_join(audio_receive_thread, nullptr);
+    pthread_join(signal_process_thread, nullptr);
+    pthread_join(classification_thread, nullptr);
+    pthread_join(output_thread, nullptr);
+    std::cout << "[RTOS] All tasks joined." << std::endl;
+}
+
+// stub task implementations
+void* audio_receive_task(void*) {
+    int frame_idx = 0;
+    while (rtos_running) {
+        std::vector<int16_t> frame;
+        AudioReadStatus status = read_audio_frame(frame);
+        if (status != AudioReadStatus::Success) {
+            std::cout << "[TASK] AudioReceiveTask: end of stream or error." << std::endl;
+            break;
+        }
+        std::cout << "[TASK] AudioReceiveTask alive, frame " << frame_idx++ << std::endl;
+        // TODO: push frame to queue
+    }
+    return nullptr;
+}
+
+void* signal_process_task(void*) {
+    int proc_idx = 0;
+    while (rtos_running) {
+        // TODO: pop frame from queue
+        std::cout << "[TASK] SignalProcessTask alive, processed frame " << proc_idx++ << std::endl;
+        usleep(10 * 1000); // 10 ms
+    }
+    return nullptr;
+}
+
+void* classification_task(void*) {
+    int class_idx = 0;
+    while (rtos_running) {
+        // TODO: wait for features
+        std::cout << "[TASK] ClassificationTask alive, label UNKNOWN, idx " << class_idx++ << std::endl;
+        usleep(30 * 1000); // 30 ms
+    }
+    return nullptr;
+}
+
+void* output_task(void*) {
+    int out_idx = 0;
+    while (rtos_running) {
+        // TODO: wait for classification result
+        std::cout << "[OUTPUT] System alive, last classification=UNKNOWN, idx " << out_idx++ << std::endl;
+        usleep(200 * 1000); // 200 ms
+    }
+    return nullptr;
+}
---
+++ src/device_firmware/rtos_tasks.h
@@ -0,0 +1,6 @@
+#pragma once
+
+// rtos lifecycle apis
+bool start_rtos_tasks();
+void request_rtos_shutdown();
+void join_rtos_tasks();
